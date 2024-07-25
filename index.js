1236602557f89c27f2d1e4698228bb11ec360983
isabelle <isabellezsspinheiro@gmail.com>
2024-07-25 16:52:43 -0300

    adicionando rotas de denuncias

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { centralRouter } from "./src/routes/central.routes.js";
+import { denunciaRouter } from "./src/routes/denuncia.routes.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/central', centralRouter);
-
+app.use('/denuncia', denunciaRouter);
 const PORT = 3000;
 
 app.listen(PORT, () => {
